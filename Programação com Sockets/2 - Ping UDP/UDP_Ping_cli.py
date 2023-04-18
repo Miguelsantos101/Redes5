2f423a91e526a0403d469f7e28e6213cf4f60390
miguel.flores <miiguelsantos05@hotmail.com>
2023-04-18 16:48:10 -0400

    RTT Mínimo, média e máximo

--- Programação com Sockets/2 - Ping UDP/UDP_Ping_cli.py
+++ Programação com Sockets/2 - Ping UDP/UDP_Ping_cli.py
@@ -1,6 +1,12 @@
 import sys, time
 from socket import socket, AF_INET, SOCK_DGRAM
 
+totalPacPerdidos = 0
+minRTT = 0
+medRTT = 0
+maxRTT = 0
+RTTtotal = 0
+
 # Get the server hostname and port as command line arguments
 argv = sys.argv                      
 host = argv[1]
@@ -14,7 +20,7 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Set socket timeout as 1 second
 #/**/ /* Seta o timeout de 1s para o socket */
-clientSocket.settimeout(1)
+clientSocket.settimeout(timeout)
 
 # Command line argument is a string, change the port into integer
 port = int(port)  
@@ -43,12 +49,23 @@ while seqNum < 10:
 		print("Reply from " + address[0] + ": " + message.decode())       
 	# Round trip time is the difference between sent and received time
 	#/**/ /* 4. Apresenta o RTT na tela */
-		print(f"{RTTr - RTTs:.4f}")
+		RTTtotal = RTTr - RTTs
+		if (RTTtotal > maxRTT):
+			maxRTT = RTTtotal
+		elif (RTTtotal < minRTT):
+			minRTT = RTTtotal
+		medRTT += RTTtotal
+
+		print(f"{RTTtotal:.4f}")
 	except:
 		# Server does not respond
 	        # Assume the packet is lost
 		print ("Request", seqNum,"timed out.")
+		totalPacPerdidos += 1
 		continue
+
+print(f"Taxa de perda: {(totalPacPerdidos / 10) * 100}%")
+print(f"RTT: \n\tMínimo = {minRTT:.4f}, Máximo = {maxRTT:.4f}, Média = {medRTT / 10:.4f}")
 
 # Close the client socket
 #/**/ /* 5. Fecha o socket */
